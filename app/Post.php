57fedbae9d03b4671a7f3a44b0a82a3857839968
hayato <hayato.pluto.4538@gmail.com>
2020-08-01 15:50:37 +0800

    make roles and permissions for users

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
-    protected $fillable = ['user_id', 'title', 'post_image', 'body'];
+    protected $guarded = [];
+    
+    // protected $fillable = ['user_id', 'title', 'post_image', 'body'];
 
     public function user(){
         return $this->belongsTo(User::class);
