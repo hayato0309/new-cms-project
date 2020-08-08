a9f822e224ab0037b19ccb8509d2eb971879ab28
hayato <hayato.pluto.4538@gmail.com>
2020-08-08 18:09:00 +0800

    display user profile page from user list

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -40,7 +40,7 @@
                     @foreach ($users as $user)
                         <tr>
                             <td>{{$user->id}}</td>
-                            <td>{{$user->username}}</td>
+                            <td><a href="{{route('user.profile.show', ['id' => $user->id])}}">{{$user->username}}</a></td>
                             <td><img class="rounded" height="40px" src="{{$user->avatar}}" alt="profile_image"></td>
                             <td>{{$user->name}}</td>
                             <td>{{$user->created_at->diffForHumans()}}</td>
--- routes/web/users.php
+++ routes/web/users.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users/{id}/profile', 'UserController@show')->name('user.profile.show');
 Route::put('/users/{id}/update', 'UserController@update')->name('user.profile.update');
 
 Route::delete('/users/{user}/delete', 'UserController@destroy')->name('user.destroy');
@@ -11,6 +10,8 @@ Route::delete('/users/{user}/delete', 'UserController@destroy')->name('user.dest
 Route::middleware('role:admin')->group(function(){
 
     Route::get('/users/', 'UserController@index')->name('users.index');
+
+    Route::get('/users/{id}/profile', 'UserController@show')->name('user.profile.show');
 
     Route::put('/users/{user}/attach', 'UserController@attach')->name('users.role.attach');
     Route::put('/users/{user}/detach', 'UserController@detach')->name('users.role.detach');
