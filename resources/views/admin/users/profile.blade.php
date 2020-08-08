670a55bab760e3a451ba11e8923ec52f0656ec05
hayato <hayato.pluto.4538@gmail.com>
2020-08-08 16:31:43 +0800

    detecting roles for the user in user profile page

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -97,7 +97,13 @@
                         <tbody>
                         @foreach ($roles as $role)
                             <tr>
-                                <th><input type="checkbox"></th>
+                                <th><input type="checkbox"
+                                            @foreach ($user->roles as $user_role)
+                                                @if($user_role->slug == $role->slug)
+                                                    checked
+                                                @endif
+                                            @endforeach>
+                                    </th>
                                 <td>{{$role->id}}</td>
                                 <td>{{$role->name}}</td>
                                 <td>{{$role->slug}}</td>
